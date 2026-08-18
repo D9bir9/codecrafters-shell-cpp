4af230cc88401bda616311794b29c6007c4e7aaf
Dabira <taiwoadejumo1234@gmail.com>
2026-08-18 17:40:05 +0100

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,9 @@ int main() {
   while (true) {
     std::cout << "$ ";
     std::cin >> command;
+    if (command == "exit") {
+      break;
+    }
     std::cout << command << ": command not found\n";
   }
 }
