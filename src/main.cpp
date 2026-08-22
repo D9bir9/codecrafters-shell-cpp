1fda54d8557fee45ec486833459ada97332e23dc
Dabira <taiwoadejumo1234@gmail.com>
2026-08-23 00:49:31 +0100

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -563,7 +563,7 @@ int main() {
   //REPL Read-Eval-Print-Loop
   while (true) {
     char* raw_input = readline("$ ");
-    update_background_jobs();
+    //update_background_jobs();
     if (raw_input == nullptr) break;
 
     std::string line(raw_input);
