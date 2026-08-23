8b9b6afca5adef38610eb51476db42cfe5837420
Dabira <taiwoadejumo1234@gmail.com>
2026-08-23 21:49:10 +0100

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -517,6 +517,14 @@ static void execute_builtin(const std::string& command, const std::vector<std::s
         const char* filename = args_[2].c_str();
         read_history(filename);
       }
+      else if (args_[1] == "-w") {
+        if ( args_.size() > 3) {
+          std::cout <<"Shell: history: too many arguments\n";
+          return;
+        }
+        const char* filename = args_[2].c_str();
+        write_history(filename);
+      }
       else {
         std::cout << "Shell: " << args_[1] << ": numeric argument required\n";
       }
