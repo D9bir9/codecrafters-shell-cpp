fd0d3e81b06c0428ea1b08bfeba8f8a2710af7d5
Dabira <taiwoadejumo1234@gmail.com>
2026-08-23 22:20:08 +0100

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -531,15 +531,13 @@ static void execute_builtin(const std::string& command, const std::vector<std::s
           return;
         }
         const auto filename = args_[2].c_str();
-        int n = 0;
         int offset = where_history();
         history_set_pos(offset);
         while (offset > 0) {
+          append_history(1, filename);
           previous_history();
           offset = where_history();
-          ++n;
         }
-        append_history(n, filename);
       }
       else {
         std::cout << "Shell: " << args_[1] << ": numeric argument required\n";
