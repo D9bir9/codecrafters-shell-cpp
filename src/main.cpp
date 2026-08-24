cac2bf32746dc1b9c9a82fe7cb555a0cf30e062e
Dabira <taiwoadejumo1234@gmail.com>
2026-08-24 11:51:47 +0100

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -246,6 +246,9 @@ static std::vector<std::string> Tokenize_input(const std::string& input_line) {
           trim(d_var);
           d_var = declared_variables[d_var];
           current_token += d_var;
+          args_.push_back(current_token);
+          current_token.clear();
+          token_has_content = false;
           is_variable = false;
           continue;
         }
